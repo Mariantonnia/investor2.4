09298969e8dce34322177d70e81e0c7cf0a6c4a5
Mariantonnia <66175651+Mariantonnia@users.noreply.github.com>
2025-07-07 06:12:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -125,12 +125,21 @@ if "historial" not in st.session_state:
 st.title("Chatbot de Análisis del perfil ESG y riesgo del inversor")
 st.markdown("""
 **Primero interactuarás con un chatbot para evaluar tu perfil ESG y de riesgo.**
+
 **Los criterios ESG (ambientales, sociales y de gobernanza) se usan para evaluar cómo una empresa impacta el medio ambiente, cómo trata a las personas (empleados, comunidad) y cómo se gestiona internamente (ética, transparencia) y ayudan a tomar decisiones responsables en inversiones y negocios.**
+
 **Al final, completarás un test tradicional de perfilado.**
+
 **Todos los datos facilitados son anónimos**
+
 **Por favor al finalizar haz click en el Botón "Enviar respuestas".**
+
 **Muchas gracias por tu colaboración.**
+
+**Los criterios ESG (ambientales, sociales y de gobernanza) se usan para evaluar cómo una empresa impacta el medio ambiente, cómo trata a las personas (empleados, comunidad) y cómo se gestiona internamente (ética, transparencia) y ayudan a tomar decisiones responsables en inversiones y negocios.**
+
 """)
+
 
 # Mostrar historial
 for mensaje in st.session_state.historial:
